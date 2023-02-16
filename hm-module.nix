e24c79fdba552255b497e2897ec13207cb631c31
Vitaliy Platonov <devroot2x@gmail.com>
2023-02-16 21:20:00 +0300

    feat: home manager module options

--- hm-module.nix
+++ hm-module.nix
@@ -1,24 +1,30 @@
-{ pkgs, ... }: {
+{ config, pkgs, lib, ... }: with lib; let cfg = config.programs.hyprland-environment; in {
 
-  xdg.configFile."hypr/hyprland.conf" = {
-    source = ./hyprland.conf;
+  options.programs.hyprland-environment = {
+    enable = mkEnableOption "hyprland-environment";
+
+    config = mkIf cfg.enable {
+      home.packages = with pkgs; [
+        bc
+        slurp
+        swayidle
+        swaylock-effects
+        notify-desktop
+        wf-recorder
+        playerctl
+      ];
+
+      xdg.configFile."hypr/hyprland.conf" = {
+        source = ./hyprland.conf;
+      };
+
+      xdg.configFile."hypr/config" = {
+        source = ./config;
+      };
+
+      xdg.configFile."hypr/scripts" = {
+        source = ./scripts;
+      };
+    };
   };
-
-  xdg.configFile."hypr/config" = {
-    source = ./config;
-  };
-
-  xdg.configFile."hypr/scripts" = {
-    source = ./scripts;
-  };
-
-  home.packages = with pkgs; [
-    bc
-    slurp
-    swayidle
-    swaylock-effects
-    notify-desktop
-    wf-recorder
-    playerctl
-  ];
 }

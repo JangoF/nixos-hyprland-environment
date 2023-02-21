193bc387723a64c755e762d760e11982aaa7edbc
Vitaliy Platonov <devroot2x@gmail.com>
2023-02-22 00:03:03 +0300

    fix: terminal has been changed

--- hyprland/scripts/terminal.sh
+++ hyprland/scripts/terminal.sh
@@ -3,9 +3,17 @@
 # <class name> <is single window> <application>
 
 if [ "$#" -eq 0 ]; then
-  alacritty --class "terminal"
+  kitty --class "terminal"
 elif [ "$#" -eq 1 ]; then
-  alacritty --class "terminal-$1"
+  kitty --class "terminal-$1"
 elif [ "$#" -gt 1 ]; then
-  alacritty --class "terminal-$1" -e ${@:2}
+  kitty --class "terminal-$1" -e ${@:2}
 fi
+
+# if [ "$#" -eq 0 ]; then
+#   alacritty --class "terminal"
+# elif [ "$#" -eq 1 ]; then
+#   alacritty --class "terminal-$1"
+# elif [ "$#" -gt 1 ]; then
+#   alacritty --class "terminal-$1" -e ${@:2}
+# fi
